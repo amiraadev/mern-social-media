fe323f5417a389e9bb180f8e3e96a1f3c22fbbee
unknown <allagui_fawzi@outlook.fr>
2023-07-06 22:57:13 +0100

    sign in with google

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
-import {Avatar,Button,Paper,Grid,Typography,Container, TextField} from '@material-ui/core'
+import {Avatar,Button,Paper,Grid,Typography,Container} from '@material-ui/core'
+// import {GoogleLogin} from 'react-google-login'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import useStyles from './authStyle'
 import Input from './Input'
+import Icon from './Icon'
 
 function Auth() {
 
@@ -17,6 +22,12 @@ function Auth() {
     const switchMode = () => {
         setisSignup((prevIsSignup) => !prevIsSignup)
         handleShowPassword(false)
+    }
+    const googleSuccess = (res) =>{
+   console.log(res);
+    }
+    const googleFailure = () =>{
+        console.log("Google sign in was unsuccessful . Try Again Later");
     }
  
   return (
@@ -38,6 +49,29 @@ function Auth() {
                     <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                     { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
                 </Grid>
+
+                <GoogleOAuthProvider clientId='1023016021226-85co271q0b34rnq3m36dk8vbodeillqc.apps.googleusercontent.com' >
+                        <GoogleLogin 
+                        clientId='GOOGLE ID'
+                        render={(renderProps) => (
+                            <Button 
+                                className={classes.googleButton} 
+                                color='primary' 
+                                fullWidth 
+                                onClick={renderProps.onClick} 
+                                disabled={renderProps.disabled}
+                                startIcon={<Icon />}
+                                variant='contained'>
+                                Google Sign In
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy='single_host_origin'
+                        />
+                </GoogleOAuthProvider>
+                
+
                 <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                          { isSignup ? 'Sign Up' : 'Sign In' }
                </Button>
--- client/src/components/Auth/authStyle.js
+++ client/src/components/Auth/authStyle.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(1),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
