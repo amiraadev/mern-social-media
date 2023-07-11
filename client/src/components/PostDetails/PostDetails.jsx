b1d0e8411f487125c5846b72cae50cb7df0de038
unknown <allagui_fawzi@outlook.fr>
2023-07-11 13:13:42 +0100

    search commit

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PostDetails() {
+    console.log("POST DETAILS ");
+  return (
+    <div>
+      POST DETAILS 
+    </div>
+  )
+}
+
+export default PostDetails
